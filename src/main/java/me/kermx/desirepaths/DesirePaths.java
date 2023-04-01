008878a87217abfe61002ff2d3815f77da39c3f8
KermX <KermitVIII@gmail.com>
2023-04-01 16:56:55 -0500

    basically rewrote the entire thing, partially async now, more efficient overall, etc.

--- src/main/java/me/kermx/desirepaths/DesirePaths.java
+++ src/main/java/me/kermx/desirepaths/DesirePaths.java
@@ -13,86 +13,48 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class DesirePaths extends JavaPlugin {
-    private static final Random random = new Random();
+
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
+    private enum modifierType{NO_BOOTS, LEATHER_BOOTS, HAS_BOOTS, FEATHER_FALLING, RIDING_HORSE, RIDING_BOAT, RIDING_PIG}
+
+
 
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("DesirePaths enabled successfully!");
         // Plugin startup logic
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-                for (Player player : Bukkit.getOnlinePlayers())
-                    DesirePaths.this.playerHandler(player);
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                playerHandler(player);
             }
-        }, 10L, 5L);
+        }, 0L, 10);
 
     }
 
     private void playerHandler(Player player){
         if (player.getGameMode() != GameMode.SURVIVAL)
             return;
-        blockBelowHandler(player.getLocation().getBlock().getRelative(BlockFace.DOWN), player);
-        blockAtFeetHandler(player.getLocation().getBlock(),player);
+        int chance = getChance(player);
+        int randomNum = random.nextInt(100);
+        Bukkit.getScheduler().runTask(this,()-> blockBelowHandler(player.getLocation().getBlock().getRelative(BlockFace.DOWN), player, chance, randomNum));
+        Bukkit.getScheduler().runTask(this,()-> blockAtFeetHandler(player.getLocation().getBlock(),player, chance, randomNum));
     }
-    private void blockAtFeetHandler(Block block, Player player){
-        if (player.isSneaking() || player.isSwimming())
-            return;
-        if (random.nextInt(100) < getChance(player) - 3 && block.getType() == Material.FARMLAND)
-            block.setType(Material.MUD);
-        if (random.nextInt(100) < getChance(player) + 30 || player.isSprinting())
-            switch (block.getType()){
-                case SNOW:
-                case GRASS:
-                case FERN:
-                    block.setType(Material.AIR);
-                    break;
-                case TALL_GRASS:
-                    block.setType(Material.GRASS);
-                    break;
-                default:
-            }
+    public static int getChance(Player player){
+        return switch (getModifier(player)) {
+            case RIDING_HORSE -> 37;
+            case RIDING_BOAT -> 99;
+            case RIDING_PIG -> 30;
+            case FEATHER_FALLING -> 10;
+            case HAS_BOOTS -> 20;
+            case LEATHER_BOOTS -> 11;
+            case NO_BOOTS -> 3;
+        };
     }
-    private void globalSwitcher(Block block){
-        switch (block.getType()){
-            case GRASS_BLOCK:
-                block.setType(Material.DIRT);
-                break;
-            case MYCELIUM:
-                block.setType(Material.ROOTED_DIRT);
-                break;
-            case PODZOL:
-            case DIRT:
-                block.setType(Material.COARSE_DIRT);
-                break;
-            case COARSE_DIRT:
-                block.setType(Material.DIRT_PATH);
-                break;
-            case SANDSTONE:
-                block.setType(Material.SAND);
-                break;
-            case CRIMSON_NYLIUM:
-            case WARPED_NYLIUM:
-                block.setType(Material.NETHERRACK);
-                break;
-            case MAGMA_BLOCK:
-                block.setType(Material.LAVA);
-                break;
-            default:
-        }
-    }
-    private void blockBelowHandler(Block block, Player player){
-        if (player.isSneaking() || player.isSwimming())
-            return;
-        if (!player.isSprinting() && random.nextInt(100) < getChance(player))
-            globalSwitcher(block);
-        if (player.isSprinting() && random.nextInt(100) < 13 + getChance(player))
-            globalSwitcher(block);
-    }
-
+    //determine modifier to use for chance
     private static modifierType getModifier(Player player){
         if (player.getVehicle() instanceof Horse)
             return modifierType.RIDING_HORSE;
@@ -103,44 +65,60 @@ public final class DesirePaths extends JavaPlugin {
         ItemStack boots = player.getInventory().getBoots();
         if (boots == null)
             return modifierType.NO_BOOTS;
-        switch (player.getInventory().getBoots().getType()){
-            case IRON_BOOTS:
-            case GOLDEN_BOOTS:
-            case DIAMOND_BOOTS:
-            case NETHERITE_BOOTS:
-                break;
-            case LEATHER_BOOTS:
-                return modifierType.LEATHER_BOOTS;
-            default:
-                return modifierType.NO_BOOTS;
-        }
-        if (boots.containsEnchantment(Enchantment.PROTECTION_FALL))
-            return modifierType.FEATHER_FALLING;
-        return modifierType.HAS_BOOTS;
-    }
-    private enum modifierType{
-        NO_BOOTS, LEATHER_BOOTS, HAS_BOOTS, FEATHER_FALLING, RIDING_HORSE, RIDING_BOAT, RIDING_PIG
-    }
-    public static int getChance(Player player){
-        switch (getModifier(player)){
-            case NO_BOOTS:
-                return 3;
-            case LEATHER_BOOTS:
-                return 11;
-            case HAS_BOOTS:
-                return 20;
-            case FEATHER_FALLING:
-                return 10;
-            case RIDING_HORSE:
-                return 37;
-            case RIDING_BOAT:
-                return 99;
-            case RIDING_PIG:
-                return 30;
-            default:
-                return 0;
+        Material bootMaterial = boots.getType();
+        Set<Material> bootMaterials = EnumSet.of(Material.IRON_BOOTS,Material.GOLDEN_BOOTS,Material.DIAMOND_BOOTS,Material.NETHERITE_BOOTS,Material.LEATHER_BOOTS);
+        Optional<Enchantment> featherFallingEnchantment = boots.getEnchantments().keySet().stream().filter(Enchantment.PROTECTION_FALL::equals).findFirst();
+        if (bootMaterials.contains(bootMaterial)){
+            if (featherFallingEnchantment.isPresent()){
+                return modifierType.FEATHER_FALLING;
+            } else {
+                return modifierType.HAS_BOOTS;
+            }
+        } else if (bootMaterial == Material.LEATHER_BOOTS){
+            return modifierType.LEATHER_BOOTS;
+        } else {
+            return modifierType.NO_BOOTS;
         }
     }
+    //Handle block at the players feet
+    private void blockAtFeetHandler(Block block, Player player, int chance, int randomNum){
+        if (!player.isSprinting() && randomNum < chance)
+            blockAtFeetSwitcher(block);
+        if (player.isSprinting())
+            blockAtFeetSwitcher(block);
+    }
+    private void blockAtFeetSwitcher(Block block){
+        Material type = block.getType();
+        block.setType(switch(type){
+            case SNOW,GRASS,FERN -> Material.AIR;
+            case TALL_GRASS -> Material.GRASS;
+            default -> type;
+        });
+    }
+
+    //Handle block below the player
+    private void blockBelowHandler(Block block, Player player, int chance, int randomNum){
+        if (player.isSneaking() || player.isSwimming())
+            return;
+        if (!player.isSprinting() && randomNum < chance)
+            blockBelowSwitcher(block);
+        if (player.isSprinting() && randomNum < 15 + chance)
+            blockBelowSwitcher(block);
+    }
+    private void blockBelowSwitcher(Block block){
+        Material type = block.getType();
+        block.setType(switch(type){
+            case GRASS_BLOCK -> Material.DIRT;
+            case DIRT, PODZOL -> Material.COARSE_DIRT;
+            case COARSE_DIRT -> Material.DIRT_PATH;
+            case SANDSTONE -> Material.SAND;
+            case CRIMSON_NYLIUM, WARPED_NYLIUM -> Material.NETHERRACK;
+            case MYCELIUM -> Material.ROOTED_DIRT;
+            case MAGMA_BLOCK -> Material.LAVA;
+            default -> type;
+        });
+    }
+
 
     @Override
     public void onDisable() {
