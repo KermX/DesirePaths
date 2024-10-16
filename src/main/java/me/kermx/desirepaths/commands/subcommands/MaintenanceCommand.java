29d7ae6c73c464f5d6be2b53e4caa243c7e3fdf1
Vladimir Kosilov <139114084+ephyrriz@users.noreply.github.com>
2024-10-16 11:49:44 +0400

    Added comments

--- src/main/java/me/kermx/desirepaths/commands/subcommands/MaintenanceCommand.java
+++ src/main/java/me/kermx/desirepaths/commands/subcommands/MaintenanceCommand.java
@@ -7,13 +7,27 @@ import org.bukkit.command.CommandSender;
 
 import java.util.List;
 
+/**
+* A class for Maintenance sub command.
+*/
 public class MaintenanceCommand implements DesirePathsSub {
     private final DesirePaths plugin;
 
+    /**
+    * Constructs this class on initialization.
+    *
+    * @param The plugin's instance
+    */
     public MaintenanceCommand(final DesirePaths plugin) {
         this.plugin = plugin;
     }
 
+    /**
+    * If the method was triggered, we process it.
+    *
+    * @param sender The sender
+    * @param args   Args of the command
+    */
     @Override
     public void onCommand(final CommandSender sender, final String[] args) {
         final boolean maintenanceMode = plugin.getToggleManager().getMaintenanceMode();
@@ -24,6 +38,13 @@ public class MaintenanceCommand implements DesirePathsSub {
         sender.sendMessage(ChatColor.GREEN + "Maintenance mode toggled " + maintenanceStatus + "!");
     }
 
+    /**
+    * When tab complete is triggered, we process it.
+    *
+    * @param sender The sender
+    * @param args   Args of the command
+    * @return Nothing
+    */
     @Override
     public List<String> onTabComplete(final CommandSender sender, final String[] args) {
         return List.of();
