22e21d4732cd269a92b10247f244999e7d0b6d25
Vladimir Kosilov <139114084+ephyrriz@users.noreply.github.com>
2024-10-16 11:47:56 +0400

    Added comments

--- src/main/java/me/kermx/desirepaths/commands/subcommands/ReloadCommand.java
+++ src/main/java/me/kermx/desirepaths/commands/subcommands/ReloadCommand.java
@@ -7,13 +7,25 @@ import org.bukkit.command.CommandSender;
 
 import java.util.List;
 
+/**
+* A class for Reload sub command.
+*/
 public class ReloadCommand implements DesirePathsSub {
     private final DesirePaths plugin;
 
+    /**
+    * Constructs this class on initialization.
+    */
     public ReloadCommand(final DesirePaths plugin) {
         this.plugin = plugin;
     }
 
+    /**
+    * If the method was triggered, we process it.
+    *
+    * @param sender The sender
+    * @param args   Args of the command
+    */
     @Override
     public void onCommand(final CommandSender sender, final String[] args) {
         plugin.reloadConfig();
@@ -21,6 +33,13 @@ public class ReloadCommand implements DesirePathsSub {
         sender.sendMessage(ChatColor.GREEN + "DesirePaths configuration reloaded!");
     }
 
+    /**
+    * When tab complete is triggered, we process it.
+    *
+    * @param sender The sender
+    * @param args   Args of the command
+    * @return Nothing
+    */
     @Override
     public List<String> onTabComplete(final CommandSender sender, final String[] args) {
         return List.of();
