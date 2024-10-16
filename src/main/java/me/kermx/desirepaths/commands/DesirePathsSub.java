9cdaab110d81270a5a9b4fa828772c079bd5b57b
Vladimir Kosilov <139114084+ephyrriz@users.noreply.github.com>
2024-10-16 11:13:09 +0400

    Added comments

--- src/main/java/me/kermx/desirepaths/commands/DesirePathsSub.java
+++ src/main/java/me/kermx/desirepaths/commands/DesirePathsSub.java
@@ -4,7 +4,16 @@ import org.bukkit.command.CommandSender;
 
 import java.util.List;
 
+/**
+* This interface represents the command and tab complete
+* triggers for a sub command. When you create a new sub
+* command, you implement this interface to it.
+*
+* Be aware that args should be moved by 1 because the
+* first argument is our sub command, not the args of
+* this sub command.
+*/
 public interface DesirePathsSub {
-    void onCommand(CommandSender sender, String[] args);
-    List<String> onTabComplete(CommandSender sender, String[] args);
+    void onCommand(CommandSender sender, String[] args); // On command trigger
+    List<String> onTabComplete(CommandSender sender, String[] args); // On Tab Complete trigger
 }
